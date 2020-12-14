bb148ec542e954d98b2cbedd5fe7ce02adb1935d
Entesar Alsowmely <ma203ea@gold.ac.uk>
2020-12-14 21:26:13 +0000

    worked a little. Added the logo and started trying to make it responsive

--- functions.php
+++ functions.php
@@ -12,8 +12,23 @@ wp_enqueue_style( 'bootstrap-css');
 function theme_add_bootstrap() {
 
 }
-
-
+ 
+// to allow for a custom logo https://developer.wordpress.org/themes/functionality/custom-logo/
+// this can simply be add_theme_support( 'custom-logo' );
+// but i want the image to be resonsive so i added this.... will need to look into it!
+function themename_custom_logo_setup() {
+	$defaults = array(
+	'height'      => 100,
+	'width'       => 400,
+	'flex-height' => true,
+	'flex-width'  => true,
+	'header-text' => array( 'site-title', 'site-description' ),
+   'unlink-homepage-logo' => true, 
+	);
+	add_theme_support( 'custom-logo', $defaults );
+   }
+   add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
+   
 add_theme_support( 'post-thumbnails' ); 
 
 add_action('wp_enqueue_scripts' ,'getMyResources', 'theme_add_bootstrap');
--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Lobster+Two|Pacifico" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Alata|Arimo:400,700|Muli|Nunito|Rubik&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
 
 <title class = "siteName"> <?php wp_title(); ?> </title>
 <?php wp_head();?>
@@ -15,18 +16,23 @@
 <!---site -header -->
 <!-- <nav class="ea-nav navbar navbar-default site-nav navbar-fixed-top" role="navigation">  -->
 <!-- Brand and toggle get grouped for better mobile display --> 
-  <div class="ea-header navbar-header"> 
-  <a class="navbar-brand siteName" href="<?php bloginfo('url')?>"><?php bloginfo('name');?> </a> 
-  </div> 
   <!-- Collect the nav links, forms, and other content for toggling --> 
   <div class="collapse navbar-collapse navbar-ex1-collapse navbar-right"> 
 
- <?php 
- 
- $args = array('theme_location' => 'primary');?>
+  <?php $args = array('theme_location' => 'primary');?>
 
-<?php wp_nav_menu($args);?>
-  <!-- </div>
+  <div class="custom-logo">
+    <?php 
+    if ( function_exists( 'the_custom_logo' ) ) {
+    the_custom_logo();
+    }?>
+  </div>
+<p class="testtt"> HEYYYY</p>
+  <div class="custom-menu">
+    <?php wp_nav_menu($args);?>
+  </div>
+</div>
+  <!-- 
 </nav>  -->
 </header>
 <body>
