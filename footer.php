a45d6f4be9daac8d51d74620dfc95d95ec67b486
Entesar Alsowmely <ma203ea@gold.ac.uk>
2020-12-21 22:34:37 +0000

    This is not final but I added a few new features. It obv doesn't work properly though

--- footer.php
+++ footer.php
@@ -1,7 +1,31 @@
-<footer>
-<p><?php  bloginfo('name')?> - &copy; <?php echo date("Y");?></p>
-</footer>
+        <footer>
+            <div class="custom-menu-in-footer">
+                <?php wp_nav_menu($args);?>
+            </div>
 
-<?php wp_footer() ?>
-</body>
+            <div id="social-media-sidebar" class="secondary">
+                <div id="social-media-sidebar-div">
+                    <?php
+                        if(is_active_sidebar('social-media-sidebar')){
+                            dynamic_sidebar('social-media-sidebar');
+                        }
+                    ?>
+                </div>
+            </div>
+
+            <div class="copyright-info">
+                <p>
+                    <?php  bloginfo('name')?> - &copy; <?php echo date("Y");?>
+                </p>
+            </div>
+        </footer>
+
+        <?php 
+            /* Always have wp_footer() just before the closing </body>
+            * tag of your theme, or you will break many plugins, which
+            * generally use this hook to reference JavaScript files.
+            */
+            wp_footer()
+        ?> 
+    </body>
 </html>
